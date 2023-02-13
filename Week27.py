f448bd91592977c96be0e39f0ca718ab460c3e85
keito089 <122423606+keito089@users.noreply.github.com>
2023-02-13 11:30:08 +0700

    Create Week27.py

---
+++ Week27.py
@@ -0,0 +1,20 @@
+x = ("input(please enter your time : " )
+
+b = (time) =  7.00 ~ 8.00 == (output("breakfast")
+
+l = (time) =  12.00 ~ 13.00 == (output("lunch")
+
+d = (time) = 18.0 ~ 19.00 == (output("dinner")
+
+else:
+  (b) > 8.00 print ("none")
+
+def main():
+    
+
+def convert(time):
+    
+
+
+if __name__ == "__main__":
+    main()
