e2eb63921ebffc48bee1b3431ab66c54137e216a
keito089 <122423606+keito089@users.noreply.github.com>
2023-02-13 11:56:11 +0700

    Update Week27.py

--- Week27.py
+++ Week27.py
@@ -1,19 +1,22 @@
-x = ("input(please enter your time : " )
 
-b = (time) =  7.00 ~ 8.00 == (output("breakfast")
-
-l = (time) =  12.00 ~ 13.00 == (output("lunch")
-
-d = (time) = 18.0 ~ 19.00 == (output("dinner")
-
-else:
-  (b) > 8.00 print ("none")
 
 def main():
+  time = input ("what time is it")                            
+  ctime= convert(time)   
+  if ctime >=7 and time <=8:
+    print("breakfast")
+  elif ctime >=12 and time <=13
+    print ("lunch")
+   elif ctime >=18 and time <=19
+    print ("dinner")
     
 
 def convert(time):
-    
+    h, m = time.split(":")
+    h = float(h)
+    m = float(m)
+    m = m/60
+    return h + m
 
 
 if __name__ == "__main__":
